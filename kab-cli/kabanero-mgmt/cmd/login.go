b01e27ea4c352a1341e5da3bae7710410ba864b0
Claudia Yan <claudia.yan@ibm.com>
2019-08-15 10:39:29 -0400

    cleaned up code to get a sort of functinal login

--- kab-cli/kabanero-mgmt/cmd/login.go
+++ kab-cli/kabanero-mgmt/cmd/login.go
@@ -16,28 +16,16 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
-
-	"github.com/spf13/viper"
+	"io/ioutil"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
-
-var kabConfig *viper.Viper
-
-func urlAccess(url string) {
-	if url == "" {
-		fmt.Printf("URLLLLLLLLLL?>>>>>>>> + " + kabConfig.GetString("KABURL"))
-	} else {
-		kabConfig.SetDefault("KABURL", url)
-	}
-}
-
-func initKab() {
-
-}
 
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
@@ -59,52 +47,36 @@ var loginCmd = &cobra.Command{
 		username := args[0]
 		password := args[1]
 		var kabURL string
-		KabEnvVar := "KABURL"
 
 		if len(args) > 2 {
 			kabURL = args[2]
-			os.Setenv(KabEnvVar, kabURL)
-			urlAccess(kabURL)
-			// fmt.Printf("SET VAR?----" + os.Getenv(KabEnvVar))
-			// cliConfig.SetDefault(KabEnvVar, kabURL)
-			// fmt.Printf("\n VIPER ACCESS ------" + cliConfig.GetString(KabEnvVar))
-
 		} else {
-			// cliConfig.AutomaticEnv()
-
-			val := kabConfig.GetString(KabEnvVar)
-			// val, present := os.LookupEnv("KABURL_MANAGEMENTCLI")
-			urlAccess("")
-			if val == "" {
-				return errors.New("No Kabanero instance url specified")
-			}
-			kabURL = val
+			return errors.New("No Kabanero instance url specified")
 		}
 
-		// client := &http.Client{
-		// 	Timeout: time.Second * 30,
-		// }
+		client := &http.Client{
+			Timeout: time.Second * 30,
+		}
 
-		// requestBody, _ := json.Marshal(map[string]string{"gituser": username, "gitpat": password})
+		requestBody, _ := json.Marshal(map[string]string{"gituser": username, "gitpat": password})
 
-		// req, err := http.NewRequest("POST", kabURL, bytes.NewBuffer(requestBody))
-		// if err != nil {
-		// 	return err
-		// }
+		req, err := http.NewRequest("POST", kabURL, bytes.NewBuffer(requestBody))
+		if err != nil {
+			return err
+		}
 
-		// req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Content-Type", "application/json")
 
-		// resp, err := client.Do(req)
+		resp, err := client.Do(req)
 
-		// if err != nil {
-		// 	fmt.Printf("The HTTP request failed with error %s\n", err)
-		// }
+		if err != nil {
+			fmt.Printf("The HTTP request failed with error %s\n", err)
+		}
 
-		// defer resp.Body.Close()
+		data, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
 
-		// data := json.Decode(resp.Body)
-
-		// fmt.Println(string(data))
+		fmt.Println(string(data))
 		fmt.Printf("USERNAME/PWD/KAB" + username + "-- " + password + "____" + kabURL)
 		return nil
 	},
