87159ac8400a82c5f271a377089fd81fb5e6cb70
Claudia Yan <claudia.yan@ibm.com>
2019-08-22 19:35:38 -0400

    adds all collections in one big list

--- kab-cli/kabanero-mgmt/cmd/list.go
+++ kab-cli/kabanero-mgmt/cmd/list.go
@@ -78,15 +78,30 @@ to quickly create a Cobra application.`,
 		Debug.log(data)
 		tWriter := new(tabwriter.Writer)
 		tWriter.Init(os.Stdout, 0, 8, 0, '\t', 0)
-		fmt.Println("Masters Collections")
-		fmt.Fprintf(tWriter, "\n%s\t%s\t", "Name", "Version")
-		fmt.Fprintf(tWriter, "\n%s\t%s\t", "----", "----")
 
-		for i := 0; i < len(data.MasterColl); i++ {
-			fmt.Fprintf(tWriter, "\n %s\t%s", data.MasterColl[i].Name, data.MasterColl[i].Version)
+		fmt.Fprintf(tWriter, "Masters Collections\n")
+		fmt.Fprintf(tWriter, "\n%s\t%s\t%s", "Name", "Version", "Collection")
+		fmt.Fprintf(tWriter, "\n%s\t%s\t%s", "----", "----", "----")
+		for i := 0; i < len(data.NewColl); i++ {
+			fmt.Fprintf(tWriter, "\n%s\t%s\t%s", data.NewColl[i].Name, data.NewColl[i].Version, "new collection")
 		}
+		for i := 0; i < len(data.KabColl); i++ {
+			fmt.Fprintf(tWriter, "\n%s\t%s\t%s", data.KabColl[i].Name, data.KabColl[i].Version, "kab collections")
+		}
+		for i := 0; i < len(data.ObsoleteColl); i++ {
+			fmt.Fprintf(tWriter, "\n%s\t%s\t%s", data.ObsoleteColl[i].Name, data.ObsoleteColl[i].Version, "obsolete collections")
+		}
+		for i := 0; i < len(data.MasterColl); i++ {
+			fmt.Fprintf(tWriter, "\n%s\t%s\t%s", data.MasterColl[i].Name, data.MasterColl[i].Version, "master collection")
+		}
+		for i := 0; i < len(data.VChangeColl); i++ {
+			fmt.Fprintf(tWriter, "\n%s\t%s\t%s", data.VChangeColl[i].Name, data.VChangeColl[i].Version, "changed collection")
+		}
+		fmt.Fprintln(tWriter)
+
 		tWriter.Flush()
-		fmt.Println("")
+
+		// fmt.Println("")
 
 		// fmt.Println(data.KabColl[0].OriginalName)
 
