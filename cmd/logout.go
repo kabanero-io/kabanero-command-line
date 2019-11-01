9e19bc33d9a5ac3ce9ecd98e291afb24e5b42d49
Claudia Yan <claudia.yan@ibm.com>
2019-11-01 15:02:50 -0400

    IBM Corp. to IBM Corporation & others in copyright on cmd files

--- cmd/logout.go
+++ cmd/logout.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 IBM Corp.
+Copyright © 2019 IBM Corporation and others. 
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
